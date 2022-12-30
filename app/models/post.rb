83e9de03aebd1697a86069b6022287306e35f55b
Adam DePaolo <amdepaolo@gmail.com>
2022-12-29 21:49:32 -0500

    commit tree fbb9ad19df86856beaf911878c20232d65acbf41

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title_contains_clickbait
+
+    private
+
+    def title_contains_clickbait
+        clickbait_array = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+        if clickbait_array.none? {|phrase| phrase.match(title)}
+            errors.add(:title, "Needs more clickbait")
+        end
+    end
 end
